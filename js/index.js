724e136bf6f7122e6c35de2be804da0a6b6af3f8
Arthur <gyulabyana@gmail.com>
2021-06-19 18:27:09 +0400

    feat: function to change slider image at interval added

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 // Change landing image
 const landingImg = document.querySelectorAll('.landing-img');
-const dots = document.querySelectorAll('.dot');
+const dots = Array.from(document.querySelectorAll('.dot'));
 
 const changeImage = (event) => {
-    const dotNumber = event.target.id.slice(event.target.id.length - 1); // Dot's id number
     const currentImg = document.querySelector('.current-landing'); // Current visible image
     const activeDot = document.querySelector('.active-dot'); // Current active dot
 
     currentImg.classList.add('prev-landing');
     currentImg.classList.remove('current-landing');
-    landingImg[dotNumber - 1].classList.add('current-landing');
+    landingImg[dots.indexOf(event.target)].classList.add('current-landing');
 
     activeDot.classList.remove('active-dot');
     event.target.classList.add('active-dot'); // Target dot becomes active
@@ -26,10 +25,37 @@ dots.forEach(el => {
 
 
 function changeImageInterval() {
+    const currentImg = document.querySelector('.current-landing');
+    const activeDot = document.querySelector('.active-dot');
 
+    currentImg.classList.add('prev-landing');
+    currentImg.classList.remove('current-landing');
+
+    if (currentImg.nextElementSibling) {
+        currentImg.nextElementSibling.classList.add('current-landing');
+    } else {
+        // If the current image is the last one.
+        landingImg[0].classList.add('current-landing');
+    }
+
+    activeDot.classList.remove('active-dot');
+
+    if (activeDot.nextElementSibling) {
+        activeDot.nextElementSibling.classList.add('active-dot');
+    } else {
+        // If the active dot is the last one.
+        dots[0].classList.add('active-dot');
+    }
+
+    setTimeout(() => {
+        // Class name will be removed when the sliding animation is over.
+        currentImg.classList.remove('prev-landing');
+    }, 500);
 }
 
-changeImageInterval();
+setInterval(() => {
+    changeImageInterval();
+}, 4000);
 
 // Setting query string as search value
 const searchBtn = document.getElementById('search-btn');
